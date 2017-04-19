e7b8aef18e2190c379f38384964e0cb2b3a5634d
IvanProdaiko94 <prodaiko94@gmail.com>
2017-04-19 15:07:56 +0300

    - updated data generator;

--- demo/generate_data.js
+++ demo/generate_data.js
@@ -11,28 +11,44 @@ const config = {
 };
 firebase.initializeApp(config);
 
+const DB = firebase.database();
+
 const chance = new Chance();
 
-const factory = function(i) {
+const peoplesFactory = function(i) {
   return {
     name: chance.name(),
     age: chance.age(),
     birthday: chance.birthday({string: true, american: false}),
     country: chance.country(),
     image: chance.avatar({fileExtension: 'jpg'}),
-    job: i > 50 ? "Frontend Engineer" : "Frontend Developer",
+    job: i > 100 ? "Frontend Engineer" : "Frontend Developer",
     cv:  chance.paragraph(),
     inSearch: i % 2 === 0,
     favoriteFramework: i % 2 ? (i > 100 ? "AngularJS" : "Angular2+")  : (i > 100 ? "React.JS" : "Vue.JS")
   }
 };
 
-const people = firebase.database().ref('people');
+const employersFactory = function(i) {
+  return {
+    country: chance.name(),
+    address: chance.address({short_suffix: true}),
+    phone: chance.phone(),
+    lookingFor: i > 100 ? "Frontend Engineer" : "Backend Engineer",
+    email: chance.email(),
+    image: chance.avatar({fileExtension: 'jpg'})
+  }
+};
+
 let counter = 199;
+const people = DB.ref('people');
+const employers = DB.ref('employers');
+
 for (let i = 0; i < 200; i++) {
-  const data = factory(i);
-  const newKey = people.push();
-  newKey.set(data).then(() => {
+  Promise.all([
+    people.push(peoplesFactory(i)),
+    employers.push(employersFactory(i))
+  ]).then(() => {
     --counter;
     if (counter === 0) process.exit(0);
   });
