c3d596290affb8ec478adb963c2f341527e68b0f
IvanProdaiko94 <prodaiko94@gmail.com>
2017-04-19 12:12:02 +0300

    - removed vars from scss;

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { NgFbSortableTableModule } from "../../../src/index";
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgFbSortableTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
