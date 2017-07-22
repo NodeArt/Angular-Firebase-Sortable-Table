012799707a4df824038bbbb377f7bdf08f2339c4
Ivan Prodaiko <i.prodaiko@nodeart.io>
2017-07-22 16:00:42 +0300

    - display;

--- src/app/table-item/table-item.component.ts
+++ src/app/table-item/table-item.component.ts
@@ -5,11 +5,7 @@ import { database } from 'firebase';
 @Component({
   selector: 'table-item',
   templateUrl: './table-item.component.html',
-  styles: [`
-    div {
-      color: red;
-    }
-  `]
+  styleUrls: ['./table-item.component.scss']
 })
 export class TableItemComponent implements ItemComponent, OnInit {
   @Input() ref: database.DataSnapshot;
@@ -17,7 +13,25 @@ export class TableItemComponent implements ItemComponent, OnInit {
 
   private item: Object;
 
+  public imageUrl: string;
+
   ngOnInit() {
-    this.item = this.ref.val()
+    this.item = this.ref.val();
+    this.getImage('thumbnail');
+  }
+
+  public loadImg(event): void {
+    event.target.style.opacity = 1;
+  }
+  public getImage(size) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://randomuser.me/api/?inc=picture', true);
+    xhr.send();
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        this.imageUrl = JSON.parse(xhr.response)['results'][0]['picture'][size];
+      }
+    };
+    xhr.onerror = () => this.imageUrl = '';
   }
 }
